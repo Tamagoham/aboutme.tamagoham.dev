865253a5a0ae4fe5bc085524f107e08ff7b97d78
Tamagoham <gleamfever797@hotmail.com>
2021-06-02 03:45:15 +0900

    :pencil2: Fixed URL - Added Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - tamagoham.dev',
-    title: 'tamagoham.dev',
+    title: 'About Tamagoham',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
